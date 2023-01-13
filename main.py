08949ac52a3c77dfa6fd17ebf498c734b7ed953b
celeste <clhartbooks@gmail.com>
2023-01-12 19:24:42 -0500

    dad created a testing playground finished code!!!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+from matplotlib.widgets import TextBox
+from matplotlib.widgets import Button
 
 
 # these are global variables
@@ -11,11 +13,24 @@ xmax = 0.5
 ymin = -1.2
 ymax = 1.2
 
-px = 200
+px = 50
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(9, 10))
+plt.subplots_adjust(bottom=0.3)
 
 cmapy = "inferno"
+
+xminbox_ax = plt.axes([0.1, 0.2 , 0.2, 0.04])
+xminbox = TextBox(xminbox_ax, 'xmin', initial=xmin)
+xmaxbox_ax = plt.axes([0.35, 0.2, 0.2, 0.04])
+xmaxbox = TextBox(xmaxbox_ax, 'xmax', initial=xmax)
+yminbox_ax = plt.axes([0.1, 0.125, 0.2, 0.04])
+yminbox = TextBox(yminbox_ax, 'ymin', initial=ymin)
+ymaxbox_ax = plt.axes([0.35, 0.125, 0.2, 0.04])
+ymaxbox = TextBox(ymaxbox_ax, 'ymax', initial=ymax)
+axbtn = plt.axes([0.6, 0.125, 0.2, 0.115])
+button = Button(axbtn, 'go')
+
 
 # this is going to make the complex matrix of starting points
 def build_input_matrix(screensize=200, xmin=-2.0, xmax=2.0, ymin=-2.0, ymax=2.0):
@@ -86,6 +101,10 @@ def change_ylims(event_ax):
     if busy:
         return
     ymin, ymax = event_ax.get_ylim()
+    xminbox.set_val(str(xmin))
+    xmaxbox.set_val(str(xmax))
+    yminbox.set_val(str(ymin))
+    ymaxbox.set_val(str(ymax))
     draw_mandy(xmin, xmax, ymin, ymax)
 
 
@@ -95,8 +114,17 @@ def connect_callbacks():
     ax.callbacks.connect('ylim_changed', change_ylims)
 
 
+def onclick(event):
+    xmin = eval(xminbox.text)
+    xmax = eval(xmaxbox.text)
+    ymin = eval(yminbox.text)
+    ymax = eval(ymaxbox.text)
+    draw_mandy(xmin, xmax, ymin, ymax)
+
+
 # this is the main code
 ax.set_autoscale_on(False)
+button.on_clicked(onclick)
 draw_mandy(xmin, xmax, ymin, ymax)
 connect_callbacks()
 plt.show()
